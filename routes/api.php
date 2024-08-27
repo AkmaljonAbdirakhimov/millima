266003c6c37ca87c6cfed88ae242544e6990bdcd
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-08-27 10:18:10 +0500

    api for social login

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
+    Route::post('/social-login', 'socialLogin');
     Route::post('/register', 'register');
     Route::post('/logout', 'logout')->middleware(['auth:sanctum']);
 });
